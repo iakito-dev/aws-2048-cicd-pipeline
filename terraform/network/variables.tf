1da90f1f0ef682d9b17e01f3ef5b79f585cafae0
Aki <sekai.110.0604@gmail.com>
2025-08-05 14:02:37 +0100

    Add initial Terraform network setup (VPC, Subnet, IGW, RouteTable)

---
+++ terraform/network/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "ap-northeast-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  default = "ap-northeast-1a"
+}
